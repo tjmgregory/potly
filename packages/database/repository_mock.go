bd06f4d32b02b66c5c9d058f3743c58b01a424e2
Theo Gregory <tjmg@outlook.com>
2021-02-12 19:10:39 +0000

    Database mock now supports being passed a value to copy First test on the client db repo passing Shifts client fields to be public

--- packages/database/repository_mock.go
+++ packages/database/repository_mock.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"reflect"
+
 	"github.com/stretchr/testify/mock"
 )
 
@@ -8,10 +10,29 @@ type RepositoryMock struct {
 	mock.Mock
 }
 
+/**
+When using this, one can provide an arguement after the expected error return type. This value
+will be cloned into arg3's (a pointer) destination.
+*/
 func (r *RepositoryMock) GetByUniqueField(arg1 string, arg2 string, arg3 interface{}) error {
 	args := r.Called(arg1, arg2, arg3)
-	// TODO: Will need to populate arg3 with the args data.
+	if args.Get(1) != nil {
+		cloneValue(args.Get(1), arg3)
+	}
 	return args.Error(0)
+}
+
+func cloneValue(source interface{}, destin interface{}) {
+	x := reflect.ValueOf(source)
+	if x.Kind() == reflect.Ptr {
+		starX := x.Elem()
+		y := reflect.New(starX.Type())
+		starY := y.Elem()
+		starY.Set(starX)
+		reflect.ValueOf(destin).Elem().Set(y.Elem())
+	} else {
+		destin = x.Interface()
+	}
 }
 
 func testRepositoryImplementsInterface() Repository {
