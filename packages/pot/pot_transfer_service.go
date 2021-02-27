dd13d85a9e3b55de4f03b101cfcf1530be4dc634
Theo Gregory <tjmg@outlook.com>
2021-02-27 12:30:58 +0000

    Adds implementatino of pot transfer service

--- packages/pot/pot_transfer_service.go
+++ packages/pot/pot_transfer_service.go
@@ -1,8 +1,11 @@
 package pot
 
 import (
+	"github.com/juju/errors"
+
 	"theodo.red/creditcompanion/packages/money"
-	"theodo.red/creditcompanion/packages/tokens/tokrepo"
+	"theodo.red/creditcompanion/packages/tokens"
+	"theodo.red/creditcompanion/packages/tokens/tokserv"
 )
 
 type PotTransferService interface {
@@ -11,5 +14,33 @@ type PotTransferService interface {
 
 type BasePotTransferService struct {
 	potRepo   PotRepository
-	tokenRepo tokrepo.TokenRepository
+	tokenRepo tokserv.TokenService
+}
+
+type PotTransferExecutor func(accessToken tokens.Token, direction money.TransactionDirection, amount money.MonetaryAmount) error
+
+var potTransferMap = map[PotProvider]PotTransferExecutor{
+	MONZO: MonzoPotTransferExecutor,
+}
+
+func (p *BasePotTransferService) TransferCash(potId string, requestorId string, direction money.TransactionDirection, amount money.MonetaryAmount) error {
+	pot, err := p.potRepo.Get(potId)
+	if err != nil {
+		return errors.Annotatef(err, "Failed to find pot %v", potId)
+	}
+
+	if pot.registeredBy != requestorId {
+		return errors.Errorf("Requestor %v who did not create pot %v attempted perform operations upon it.", requestorId, potId)
+	}
+
+	token, err := p.tokenRepo.GetTokenById(pot.accessTokenId)
+	if err != nil {
+		return errors.Annotatef(err, "Failed to find token %v for pot %v", pot.accessTokenId, potId)
+	}
+
+	return potTransferMap[pot.potProvider](*token, direction, amount)
+}
+
+func MonzoPotTransferExecutor(accessToken tokens.Token, direction money.TransactionDirection, amount money.MonetaryAmount) error {
+	return nil
 }
