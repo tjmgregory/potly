c875f8d5a7d2dc86cfa4d26a77fdc3bcec07e982
Theo Gregory <tjmg@outlook.com>
2021-03-24 20:47:50 +0000

    Adds a test for multiple transactions at once

--- functions/moveCashBetweenPots/transaction_processor_test.go
+++ functions/moveCashBetweenPots/transaction_processor_test.go
@@ -58,3 +58,68 @@ func TestProcessesATransactionSuccessfully(t *testing.T) {
 
 	assert.Nil(t, err)
 }
+
+func TestProcessesMultipleLinkedAccountsWithMultiplePotsEach(t *testing.T) {
+	// Given we build the processor with mocks
+	clientRepoMock := new(clirepo.ClientRepositoryMock)
+	potTransferServiceMock := new(pot.PotTransferServiceMock)
+
+	processorUnderTest := ParallelTransactionProcessor{
+		clientRepo:         clientRepoMock,
+		potTransferService: potTransferServiceMock,
+	}
+
+	funcArgTransaction := credtrack.CreditTransaction{
+		Id:             "transaction-123",
+		CreditSourceId: "credit-source-123",
+		CreatedAt:      "created-at-timestamp",
+		LinkedClients: map[string]float32{
+			"client-123": 0.6,
+			"client-456": 0.4,
+		},
+		Total: money.MonetaryAmount{
+			Value:    160.0,
+			Currency: money.GBP,
+		},
+		TransactionDirection: money.DEBIT,
+		Description:          "some-description",
+		TransactedAt:         "transacted-at-timestamp",
+	}
+
+	// And we can find each client
+	clientRepoMock.On("Get", "client-123").Return(&clients.Client{
+		Id:        "client-123",
+		Email:     "test@email.com",
+		CreatedAt: "client-created-timestamp",
+		Pots: map[string]float32{
+			"pot-111": 0.8,
+			"pot-112": 0.2,
+		},
+	}, nil)
+
+	// and this client has a greater cumulative multiplier than x1.0 on their movements
+	clientRepoMock.On("Get", "client-456").Return(&clients.Client{
+		Id:        "client-456",
+		Email:     "test@email.com",
+		CreatedAt: "client-created-timestamp",
+		Pots: map[string]float32{
+			"pot-221": 1.0,
+			"pot-222": 1.2,
+		},
+	}, nil)
+
+	// And the tranfers all succeed
+	// 160 * 0.6 * 0.8
+	potTransferServiceMock.On("TransferCash", "pot-111", "client-123", money.CREDIT, money.MonetaryAmount{Value: 76.8, Currency: money.GBP}, "client-123pot-111transaction-123").Return(nil)
+	// 160 * 0.6 * 0.2
+	potTransferServiceMock.On("TransferCash", "pot-112", "client-123", money.CREDIT, money.MonetaryAmount{Value: 19.2, Currency: money.GBP}, "client-123pot-112transaction-123").Return(nil)
+	// 160 * 0.4 * 1.0
+	potTransferServiceMock.On("TransferCash", "pot-221", "client-456", money.CREDIT, money.MonetaryAmount{Value: 64.0, Currency: money.GBP}, "client-456pot-221transaction-123").Return(nil)
+	// 160 * 0.4 * 1.2
+	potTransferServiceMock.On("TransferCash", "pot-222", "client-456", money.CREDIT, money.MonetaryAmount{Value: 76.8, Currency: money.GBP}, "client-456pot-222transaction-123").Return(nil)
+
+	// When we call the function, it succeeds without error
+	err := processorUnderTest.Process(funcArgTransaction)
+
+	assert.Nil(t, err)
+}
