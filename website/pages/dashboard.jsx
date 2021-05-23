2217f2bf613d1ad952434bfa7f96d46477b99122
Theo Gregory <tjmg@outlook.com>
2021-05-23 19:45:19 +0100

    Finishes working Magiclink example.

--- website/pages/dashboard.jsx
+++ website/pages/dashboard.jsx
@@ -1,4 +1,12 @@
+import Layout from '../components/layout'
+import useAuth from '../hooks/useAuth'
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>
+    const { user, loading } = useAuth()
+    return (
+        <Layout>
+            <h1>Dashboard</h1>
+            {loading ? 'Loading...' : JSON.stringify(user)}
+        </Layout>
+    )
 }
