8074ea5830bb2ca78d5452625dc2d6a1d88b409a
Theo Gregory <tjmg@outlook.com>
2022-01-29 07:57:26 +0000

    Clean up the login page

--- website/pages/login/index.tsx
+++ website/pages/login/index.tsx
@@ -1,23 +1,20 @@
 import Head from 'next/head'
-import Layout from '../../components/layout'
 import { linkToLoginCallback } from '../../lib/links'
 
 const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUB_KEY
 
 const Login: React.FunctionComponent = () => {
   return (
-    <Layout>
-      <Head>
-        <title>Login</title>
-        <script
-          src="https://auth.magic.link/pnp/login"
-          data-magic-publishable-api-key={MAGIC_PUBLIC_KEY}
-          data-terms-of-service-uri="/path/to/your/terms-of-service"
-          data-privacy-policy-uri="/path/to/your/privacy-policy"
-          data-redirect-uri={linkToLoginCallback()}
-        ></script>
-      </Head>
-    </Layout>
+    <Head>
+      <title>Login</title>
+      <script
+        src="https://auth.magic.link/pnp/login"
+        data-magic-publishable-api-key={MAGIC_PUBLIC_KEY}
+        data-terms-of-service-uri="/path/to/your/terms-of-service"
+        data-privacy-policy-uri="/path/to/your/privacy-policy"
+        data-redirect-uri={linkToLoginCallback()}
+      ></script>
+    </Head>
   )
 }
 
