f18e12adb5d4fcecd4d2d2ab870252fdb5d99e5f
Theo Gregory <tjmg@outlook.com>
2022-04-26 09:04:26 +0100

    fix: avatar styles

--- website/components/Avatar.tsx
+++ website/components/Avatar.tsx
@@ -5,32 +5,32 @@ import styled from 'styled-components'
 // TODO: This is undefined
 console.log(AvatarPrimitive)
 export const Avatar = styled(AvatarPrimitive.Root)`
-  display: 'inline-flex';
-  align-items: 'center';
-  justify-content: 'center';
-  vertical-align: 'middle';
-  overflow: 'hidden';
-  user-select: 'none';
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  overflow: hidden;
+  user-select: none;
   width: 45;
   height: 45;
-  border-radius: '100%';
+  border-radius: 100%;
   background-color: ${blackA.blackA3};
 `
 
 export const AvatarImage = styled(AvatarPrimitive.Image)`
-  width: '100%';
-  height: '100%';
-  object-fit: 'cover';
-  border-radius: 'inherit';
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
 `
 
 export const AvatarFallback = styled(AvatarPrimitive.Fallback)`
-  width: '100%';
-  height: '100%';
-  display: 'flex';
-  align-items: 'center';
-  justify-content: 'center';
-  background-color: 'white';
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
   color: ${violet.violet11};
   font-size: 15;
   line-height: 1;
