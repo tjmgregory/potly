acb0241dadb53175792d7c0e4ed54f3ef44d14ca
Theo Gregory <tjmg@outlook.com>
2022-04-26 09:06:29 +0100

    chore: remove unnecessary links

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -6,12 +6,6 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,900&display=swap"
             rel="stylesheet"
