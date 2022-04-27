34e5a437bc1ef81b3e21aa54e63c0955e8e46104
Theo Gregory <tjmg@outlook.com>
2022-04-27 07:39:54 +0100

    feat: declaration merging of styled-components theme type

---
+++ website/components/ThemeProvider/styled.d.ts
@@ -0,0 +1,5 @@
+import {} from 'styled-components'
+import { Theme } from './theme'
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
--- website/components/ThemeProvider/theme.ts
+++ website/components/ThemeProvider/theme.ts
@@ -1,21 +1,8 @@
-import { ThemeProvider } from 'styled-components'
-
-type Theme = React.ComponentProps<typeof ThemeProvider>['theme']
-
-const theme: Theme = {
+export const lightTheme = {
   fonts: {
     system: 'system-ui',
   },
-  colors: {
-    hiContrast: 'hsl(206,10%,5%)',
-    loContrast: 'white',
-    gray100: 'hsl(206,22%,99%)',
-    gray200: 'hsl(206,12%,97%)',
-    gray300: 'hsl(206,11%,92%)',
-    gray400: 'hsl(206,10%,84%)',
-    gray500: 'hsl(206,10%,76%)',
-    gray600: 'hsl(206,10%,44%)',
-  },
+  colors: {},
   fontSizes: {
     1: '1rem',
     2: '1.1rem',
@@ -82,4 +69,9 @@ const theme: Theme = {
   },
 }
 
-export default theme
+export const darkTheme: typeof lightTheme = {
+  ...lightTheme,
+  colors: {},
+}
+
+export type Theme = typeof lightTheme
