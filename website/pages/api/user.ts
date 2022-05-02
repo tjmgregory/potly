a36c229b7469991044d3bec62c36aa32c8bff39e
Theo Gregory <tjmg@outlook.com>
2022-05-02 17:43:26 +0100

    feat: adds planetscale connection via prisma

--- website/pages/api/user.ts
+++ website/pages/api/user.ts
@@ -1,7 +1,11 @@
 import { authedApi } from '@/lib/auth'
+import prisma from '@/lib/prisma'
 
 export default authedApi(async ({ res, user }) => {
   // TODO: Do a DB lookup to get other use data
+  const users = await prisma.user.findMany()
+  console.log('theo-7305', { users })
+
   res.json({
     magicUser: user,
     name: 'Steve Buscemi',
