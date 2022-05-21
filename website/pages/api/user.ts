614dfb16b52b91db22d53642bc172873ca641d36
Theo Gregory <tjmg@outlook.com>
2022-05-21 18:24:13 +0100

    docs: improves comment around user api

--- website/pages/api/user.ts
+++ website/pages/api/user.ts
@@ -1,7 +1,11 @@
 import { authedApi } from '@/lib/userAuth'
 
-// TODO: This endpoint is not somewhat redundant as all user information is
-// stored in the user session token.
+/**
+ * At present, this API simply decrypts the contents of the
+ * user session token and returns the user.
+ * 200: Success.
+ * 401: Not logged in.
+ */
 export default authedApi(async ({ res, sessionToken }) => {
   res.status(200).json(sessionToken.user)
 })
